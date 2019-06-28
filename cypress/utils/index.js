3834eacf8f734531c34bf2d991062b7c1656e0a8
Saturnino Abril <saturnino.abril@gmail.com>
2019-06-29 02:43:06 +0800

    fix email verification

--- cypress/utils/index.js
+++ cypress/utils/index.js
@@ -5,5 +5,20 @@ export function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
 }
 
-export const reUrl = /(https?:\/\/[^ ]*)/;
+export function getEmailUrl(baseUrl) {
+    if (baseUrl === 'http://localhost:8065') {
+        return 'http://localhost:9000/api/v1/mailbox';
+    }
 
+    return `${baseUrl}/mail`;
+}
+
+export function getEmailMessageSeparator(baseUrl) {
+    if (baseUrl === 'http://localhost:8065') {
+        return '\r\n';
+    }
+
+    return '\n';
+}
+
+export const reUrl = /(https?:\/\/[^ ]*)/;
